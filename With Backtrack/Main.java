394c6e92da4c30165bf8c9b1c07066654f1a6650
Eric Cheah <66048503+thyyl@users.noreply.github.com>
2021-03-31 16:13:34 +0800

    Update Main.java

--- With Backtrack/Main.java
+++ With Backtrack/Main.java
@@ -1,6 +1,7 @@
 class Main {
     static int NUM_LINE = 2;
     static int NUM_STATION;
+    static int counter = 0;
     static int[][] bestPathway;
     static int bestExit;
 
@@ -13,29 +14,30 @@ class Main {
         return Math.min(a, b);
     }
 
-    static int minExit(int a, int b) {
-        bestExit = (a < b) ? 0 : 1;
-        return Math.min(a, b);
-    }
+    public static int AssemblyLine(int[][] stationCost, int[][] transferCost, int[] entryCost, int[] exitCost) {
+        int n = stationCost[0].length;
 
-    static int AssemblyLine(int[][] stationCost, int[][] transferCost, int[] entryCost, int[] exitCost) {
-        int[] T1 = new int [NUM_STATION];
-        int[] T2 = new int[NUM_STATION];
-        int i;
+        int first = entryCost[0] + stationCost[0][0];
+        int second = entryCost[1] + stationCost[1][0];
 
-        T1[0] = entryCost[0] + stationCost[0][0];
-        T2[0] = entryCost[1] + stationCost[1][0];
+        for (int i = 1; i < n; i++) {
+            int up = min(first + stationCost[0][i], second + transferCost[1][i] + stationCost[0][i], 0, i),
+                    down = min(second + stationCost[1][i], first + transferCost[0][i] + stationCost[1][i], 1, i);
 
-        for (i = 1; i < NUM_STATION; ++i) {
-            T1[i] = min(T1[i - 1] + stationCost[0][i],T2[i - 1] + transferCost[1][i] + stationCost[0][i], 0, i);
-            T2[i] = min(T2[i - 1] + stationCost[1][i],T1[i - 1] + transferCost[0][i] + stationCost[1][i],1, i);
+            first = up;
+            second = down;
         }
+        counter++;
 
-        return minExit(T1[NUM_STATION - 1] + exitCost[0],T2[NUM_STATION - 1] + exitCost[1]);
+        first += exitCost[0];
+        second += exitCost[1];
+        
+        return Math.min(first, second);
     }
 
     static void printPathway() {
         int i = bestExit;
+        counter++;
         System.out.println("Exit: Assembly Line " + (bestExit + 1) + " Station " + NUM_STATION);
 
         for (int j = NUM_STATION - 1; j > 0; j--) {
@@ -53,8 +55,8 @@ class Main {
 
     public static void main (String[] args) {
         int upperBound = 50;
-        int lowerBound = 0;
-        int maxStation = 5;
+        int lowerBound = 1;
+        int maxStation = 10;
         int minStation = 3;
         int intRandom = (int)(Math.random() * (maxStation - minStation + 1) + minStation);
         bestPathway = new int[2][intRandom];
@@ -66,13 +68,12 @@ class Main {
         int[] entryCost = new int [NUM_LINE];
         int[] exitCost = new int[NUM_LINE];
 
-
-        for (int i = 0; i < NUM_LINE; i++) {
+        for (int i = 0; i < NUM_LINE; i++, counter += 2) {
             entryCost[i] = (int)(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
             exitCost[i] = (int)(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
         }
 
-        for (int j = 0; j < intRandom; j++) {
+        for (int j = 0; j < NUM_STATION; j++, counter += 2) {
             stationCost[0][j] = (int)(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
             stationCost[1][j] = (int)(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
             transferCost[0][j] = (int)(Math.random() * (upperBound - lowerBound + 1) + lowerBound);
@@ -81,5 +82,7 @@ class Main {
 
         System.out.println(AssemblyLine(stationCost, transferCost, entryCost, exitCost));
         printPathway();
+        System.out.println("Counter: " + counter);
+        System.out.println("Station: " + NUM_STATION);
     }
 }
